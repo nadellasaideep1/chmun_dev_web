521ccb25817f2c0177e8e679af52202e6362cf7f
nadellasaideep1 <64152721+nadellasaideep1@users.noreply.github.com>
2025-05-25 22:25:11 +0530

    Update action.js

--- action.js
+++ action.js
@@ -1,8 +1,32 @@
-        function showLetter() {
-            const principalLetter = document.getElementById('principalLetter');
-            const secretaryLetter = document.getElementById('secretaryLetter');
-            principalLetter.style.display = 'block';
-            secretaryLetter.style.display = 'block';
-            document.body.style.overflow = 'auto';
-            principalLetter.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
+ function showAbout() {
+      const about = document.getElementById("aboutSection");
+      about.style.display = "block";
+      document.body.style.overflow = "auto";
+      about.scrollIntoView({ behavior: "smooth", block: "start" });
+      typeWriter()
+
+    }
+var i = 0;
+var txt = 'We aim to build a new generation of Indian leaders through';
+var i2 = 0;
+var txt2 = 'diplomacy.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  else{
+    typeWriter2()
+  }
+}
+
+function typeWriter2() {
+  if (i2 < txt2.length) {
+    document.getElementById("demo2").innerHTML += txt2.charAt(i2);
+    i2++;
+    setTimeout(typeWriter, speed);
+  }
+}
